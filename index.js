fb9e1c24ceab3fb0bbe3bcda56c1394994f5cdcd
Renzo Espinosa Barcos <renzoespinosa.b@gmail.com>
2024-09-04 20:05:51 -0300

    front ready for deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { dbConnection } = require('./database/config')
 const cors = require('cors')
+const path = require('path')
 require('dotenv').config()
 
 console.log(process.env.PORT)
@@ -23,6 +24,10 @@ app.use(express.json())
 //Rutas
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
+
+app.use('*', (req, res)=>{
+    res.send( path.join(__dirname, 'public/index.html') )
+})
 
 app.listen( process.env.PORT, ()=>{
     console.log('servidor corriendo en puerto '+ process.env.PORT)
